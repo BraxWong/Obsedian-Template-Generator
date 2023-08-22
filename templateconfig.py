c6c895d5007a8a32ff8d6f44429597e41bdf207e
Brax Wong <ghostenjob@gmail.com>
2023-08-22 22:24:32 +0100

    Updating .gitignore file.

--- templateconfig.py
+++ templateconfig.py
@@ -1,4 +1,5 @@
 import customtkinter
+from tkinter import messagebox
 
 class config(customtkinter.CTk):
 
@@ -7,7 +8,21 @@ class config(customtkinter.CTk):
         self.geometry("1000x1000")
         self.title("Template Configuration")
         self.resizeable(False,False)
+        submitButton = customtkinter.CTkButton(self, text = "Submit", command = self.submitTemplate)
 
     def __init__(self):
         super().__init__()
         self.initializeWidget()
+
+    def submitTemplate(self):
+        if self.validateInput() == "TRUE":
+            print("YAS QUEEN")
+        else
+           messagebox.showerror(title = "Inputs not found", message = "Error: One or more inputs not found.")             
+           
+
+    def validateInput(self):
+        return "TRUE"
+
+
+        
