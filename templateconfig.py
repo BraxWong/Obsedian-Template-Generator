d4facb7d660969ae5047f9193d4238e6b855cf1a
Brax Wong <ghostenjob@gmail.com>
2023-08-23 22:15:02 +0100

    Adding layout config.

--- templateconfig.py
+++ templateconfig.py
@@ -9,9 +9,13 @@ class config(customtkinter.CTk):
         self.title("Template Configuration")
         self.resizeable(False,False)
         submitButton = customtkinter.CTkButton(self, text = "Submit", command = self.submitTemplate)
-
+        ticketNameLabel = customtkinter.CTkLabel(self, text = "Name of the ticket", font = ("American Typewriter"),25)
+        ticketNumberLabel = customtkinter.CTkLabel(self, text = "Number of the ticket", font = ("American Typewriter"),25)
+        
     def __init__(self):
         super().__init__()
+        self.ticketNumber = ""
+        self.ticketName = ""
         self.initializeWidget()
 
     def submitTemplate(self):
@@ -24,5 +28,12 @@ class config(customtkinter.CTk):
     def validateInput(self):
         return "TRUE"
 
+    def grid_configurate(self):
+       self.grid_rowconfigure(0, weight = 1)
+       self.grid_rowconfigure(1, weight = 1)
+       self.grid_rowconfigure(2, weight = 1)
+       self.grid_rowconfigure(3, weight = 1)
+       self.grid_columnconfigure(0, weight = 1)
+       self.grid_columnconfigure(1, weight = 1)
 
         
