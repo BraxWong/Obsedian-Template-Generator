eaa55d7cc7a0dd775ec16ad887c2b5229e835f93
Brax Wong <ghostenjob@gmail.com>
2023-08-27 11:43:31 +0100

    Implemented random seed generator.

---
+++ random_seed_generator.py
@@ -0,0 +1,19 @@
+import random
+
+class SeedGenerator():
+
+    def __init__(self):
+        self.seed = ""
+        self.character = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+        self.generator()
+         
+
+    def generator(self):
+        for x in range(15):
+            if random.randint(0,1) == 1:
+                self.seed += str(random.randint(0,9))
+            else:
+                self.seed += self.character[random.randint(0,25)] 
+        print(self.seed)
+            
+            
