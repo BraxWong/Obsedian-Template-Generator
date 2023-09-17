7cc3781abcbad4c6b99a53c5a55a1ddc61a976e2
Brax Wong <ghostenjob@gmail.com>
2023-09-17 21:57:51 +0100

    Updating build script to install python if not installed. Moved all python scripts to src directory

--- MacOS/build.sh
+++ MacOS/build.sh
@@ -1,4 +1,35 @@
 #!/bin/sh
+
+#  ╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
+#  ┃                                                          ┃
+#  ┃      Checking if python has been installed on local      ┃
+#  ┃               machine. If not, install it.               ┃
+#  ┃                                                          ┃
+#  ╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
+
+if command -v python3 &> /dev/null ; then
+  echo "Python is installed."
+else
+  sudo apt-get update
+  sudo apt-get install python3
+fi
+
+#  ╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
+#  ┃                                                          ┃
+#  ┃             #Installing project dependencies             ┃
+#  ┃                                                          ┃
+#  ╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
+
 pip install customtkinter
 pip install tk
 pip install pathvalidate
+pip install pyinstaller
+
+#  ╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
+#  ┃                                                          ┃
+#  ┃         #Building the project using pyinstaller          ┃
+#  ┃                                                          ┃
+#  ╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
+
+cd ..
+pyinstaller --noconfirm --onedir --windowed src/main.py
