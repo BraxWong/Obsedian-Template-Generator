b90b2af2c082ec70bc6b8fd227e37a4b311fb616
Brax Wong <ghostenjob@gmail.com>
2023-10-23 20:21:34 +0100

    Completed implementation for nodes section of canvas file

--- src/Canvas.py
+++ src/Canvas.py
@@ -7,8 +7,15 @@ def createCanvasFile(fileName, list_of_nodes, list_of_node_types):
         return
     canvaFile = open(fileName, "a")
     canvaFile.write("{\n    \"nodes\":[\n")
-    for x in list_of_nodes:
-        canvaFile.write("       {\"type\":\"\"}\n")
-createCanvasFile("testing.canva", {"HI","Bye"}, {"hi","bye"})
+
+    #Writing nodes section of the canvas file
+    for x in range(len(list_of_nodes)):
+        canvaFile.write(f"       {{\"type\":\"{list_of_node_types[x]}\",\"{list_of_node_types[x]}\":\"{list_of_nodes[x]}\"}}")
+        if x != len(list_of_nodes) - 1:
+            canvaFile.write(",\n")
+        else:
+            canvaFile.write("\n     ],")
+
+createCanvasFile("testing.canva", ["HI","Bye"], ["hi","bye"])
    
     
