7a4d9e97eac8a46ca78bfd27719838008b6414f9
Brax Wong <ghostenjob@gmail.com>
2023-10-25 20:01:21 +0100

    Completing canva file creation.

--- src/Canvas.py
+++ src/Canvas.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-def createCanvasFile(fileName, list_of_nodes, list_of_node_types):
+def createCanvasFile(fileName, list_of_nodes, list_of_node_types, connect, list_of_edges):
     if len(list_of_nodes) != len(list_of_node_types):
         print("No good")
         return
@@ -16,6 +16,14 @@ def createCanvasFile(fileName, list_of_nodes, list_of_node_types):
         else:
             canvaFile.write("\n     ],")
 
-createCanvasFile("testing.canva", ["HI","Bye"], ["hi","bye"])
+    if connect:
+        canvaFile.write(f"  \"edges\":[")
+        for x in range(len(list_of_edges)):
+            canvaFile.write(f"      {{\"id\":\"{list_of_edges[x].id}\",\"fromNode\":\"{list_of_edges[x].fromNode}\",\"fromSide\":\"{list_of_edges[x].fromSide}\",\"toNode\":\"{list_of_edges[x].toNode}\",\"toSide\":\"list_of_edges[x].toSide\"}}")
+            if x != len(list_of_edges) - 1:
+                canvaFile.write(",\n")
+            else: 
+                canvaFile.write("\n     ]\n}")
+
    
     
